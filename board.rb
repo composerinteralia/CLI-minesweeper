fe4cc317a769ffc1789d953a7dbc0bf1fca226f8
Daniel Colson <danieljamescolson@gmail.com>
2015-12-04 11:55:39 -0500

    Add Board#populate.

--- board.rb
+++ board.rb
@@ -1,5 +1,14 @@
 class Board
+  attr_reader :grid
+
   def initialize(size = 9)
     @grid = Array.new(size) { Array.new(size) }
+    populate
+  end
+
+  def populate
+    grid.map! do |row|
+      row.map! { |tile| Tile.new(:bomb) }
+    end
   end
 end
