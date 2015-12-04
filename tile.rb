26d478536477ff5586e0d084832f9208d428fbb1
Daniel Colson <danieljamescolson@gmail.com>
2015-12-04 11:22:18 -0500

    Add Tile#flag.

--- tile.rb
+++ tile.rb
@@ -1,15 +1,20 @@
 class Tile
-  attr_reader :type, :board, :revealed
+  attr_reader :type, :board, :revealed, :flagged
 
   def initialize(type, board = nil)
     @type = type
     @board = board
     @revealed = false
+    @flagged = false
   end
 
   def reveal
     @revealed = true
     type
+  end
+
+  def flag
+    @flagged = !flagged
   end
 
   def bombed?
